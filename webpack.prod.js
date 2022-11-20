86ffa2a12e27fab379713812e807213619fd000f
Hans <classsickpeople@gmail.com>
2022-11-20 22:15:47 +0800

    Memperbaiki syntax

--- webpack.prod.js
+++ webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = merge(common, {
                     {
                         loader: 'babel-loader',
                         options: {
-                            "presets": ["@babel/preset-env", "@babel/preset-react"]
+                            presets: ['@babel/preset-env', '@babel/preset-react']
                         }
                     }
                 ]
