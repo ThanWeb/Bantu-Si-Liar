e85d68646d8da0a7da5423e5af071283dd3f4f6b
Hans <classsickpeople@gmail.com>
2022-11-21 20:25:52 +0800

    Menambahkan beberapa page dan menu navigasi

--- src/script/App.js
+++ src/script/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Navigation from './component/Navigation'
 import HomePage from './page/HomePage'
+import ProfilePage from './page/ProfilePage'
+import LoginPage from './page/LoginPage'
+import RegisterPage from './page/RegisterPage'
 
 function App () {
     return (
         <div>
             <header>
+                <Navigation />
             </header>
             <main>
                 <Routes>
-                    <Route path="/" element={<HomePage />} />
+                    <Route path='/' element={<HomePage />} />
+                    <Route path='/login' element={<LoginPage />} />
+                    <Route path='/register' element={<RegisterPage />} />
+                    <Route path='/profile' element={<ProfilePage />} />
                 </Routes>
             </main>
             <footer>
---
+++ src/script/page/RegisterPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function RegisterPage () {
+    return <h1>RegisterPage</h1>
+}
+
+export default RegisterPage
