86904814a4a34fbc9f92b042534c036f8dc6edc9
Hans <classsickpeople@gmail.com>
2022-11-21 17:06:59 +0800

    Memindahkan file index.js dan App.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,4 @@
-import './style/style.scss'
+import '../style/style.scss'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const path = require('path')
 
 module.exports = {
     entry: {
-        app: path.resolve(__dirname, 'src/index.js')
+        app: path.resolve(__dirname, 'src/script/index.js')
     },
     output: {
         filename: '[name].bundle.js',
