1b5ec66609123939b2d5d3f256f9340914fd5560
putriroslin <putriroslin1@gmail.com>
2022-11-26 00:46:38 +0700

    mengupdate code di file RegisterPage.js

--- src/script/page/RegisterPage.js
+++ src/script/page/RegisterPage.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Register from '../component/Register'
 
 function RegisterPage () {
-    return <h1>RegisterPage</h1>
+    const navigate = useNavigate()
+    async function onRegisterHandler (user) {
+        const { error } = await Register(user)
+        if (!error) {
+            navigate('/')
+        }
+    }
+    return (
+        <section className='pageRegister'>
+            <h1>SELAMAT DATANG</h1>
+            <Register Register={onRegisterHandler}/>
+        </section>
+
+    )
 }
 
 export default RegisterPage
