06b86484e34d69f011ff2892c2e7f80ebbaa09bd
ocid13 <haidakhoirurrosyid@gmail.com>
2022-11-30 16:41:38 +0700

    menambahkan profil page

--- src/script/page/ProfilePage.js
+++ src/script/page/ProfilePage.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
 function ProfilePage () {
-    return <h1>ProfilePage</h1>
+    return <>
+        <div className='container'>
+            <div className='profile'>
+                <h1>Profil</h1>
+                <img src='https://cdn-icons-png.flaticon.com/512/61/61173.png' alt='icon'/>
+
+                <div className='input-profile'>
+                    <div className='card'>
+                        <label htmlFor='email'>Email</label>
+                        <input type='text' id='email' />
+                    </div>
+                    <div className='card'>
+                        <label htmlFor='username'>Username</label>
+                        <input type='text' id='username' />
+                    </div>
+                    <div className='card'>
+                        <label htmlFor='nama'>Nama</label>
+                        <input type='text' id='nama' />
+                    </div>
+                    <div className='card'>
+                        <label htmlFor='alamat'>Alamat</label>
+                        <input type='text' id='alamat' />
+                    </div>
+                    <div className='card'>
+                        <label htmlFor='noHp'>No HP</label>
+                        <input type='text' id='noHp' />
+                    </div>
+                    <div className='input_submit'>
+                        <button type='submit'>SIMPAN</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default ProfilePage
