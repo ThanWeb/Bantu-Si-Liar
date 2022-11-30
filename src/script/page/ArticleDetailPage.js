05de554e1e183547b8c29e128609fb127e319a5a
Hans <classsickpeople@gmail.com>
2022-11-30 21:32:18 +0800

    Menambahkan Article Detail Page

---
+++ src/script/page/ArticleDetailPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import getArticleBody from '../utils/getArticleBody'
+
+function ArticleDetailPage () {
+    const arrayOfText = getArticleBody(body)
+    return (
+        <div>
+            {
+                arrayOfText.contents.map((text, index) => {
+                    return arrayOfText.tags[index - 1] === 'h' ? <h3>{text}</h3> : <p>{text}</p>
+                })
+            }
+        </div>
+    )
+}
+
+export default ArticleDetailPage
