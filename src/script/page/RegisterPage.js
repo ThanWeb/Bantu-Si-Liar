783c687787617127ba9ad70392b9558bc33033e4
Gusti Ayu Istiara Bukian <istiaraayu8@gmail.com>
2022-12-01 22:36:02 +0700

    Update RegisterPage.js

--- src/script/page/RegisterPage.js
+++ src/script/page/RegisterPage.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Register from '../component/Register'
+import logo from '../img/LogoBantu.png'
 
 function RegisterPage () {
-    return <h1>RegisterPage</h1>
+    const navigate = useNavigate()
+    async function onRegisterHandler (user) {
+        const { error } = await Register(user)
+        if (!error) {
+            navigate('/')
+        }
+    }
+    return (
+        <section className='page-register'>
+            <header>
+                <img src={logo} alt='Logo Bantu Si Liar'></img>
+                <h1>SELAMAT DATANG</h1>
+            </header>
+            <Register Register={onRegisterHandler}/>
+        </section>
+
+    )
 }
 
 export default RegisterPage
