266092289a0e3a12651d1794a21233b672f33dca
Gusti Ayu Istiara Bukian <istiaraayu8@gmail.com>
2022-12-01 22:36:12 +0700

    Update LoginPage.js

--- src/script/page/LoginPage.js
+++ src/script/page/LoginPage.js
@@ -1,7 +1,40 @@
+/* eslint-disable react/react-in-jsx-scope */
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import InputLogin from '../component/InputLogin'
+import Logo from '../img/LogoBantu.png'
+import AnimasiLogin from '../img/animasi-login.png'
+import { login } from '../utils/network-data'
 
 function LoginPage () {
-    return <h1>LoginPage</h1>
+    const navigate = useNavigate()
+    const onLogin = async ({ email, password }) => {
+        const { error } = await login({ email, password })
+
+        if (!error) {
+            navigate('/')
+        }
+    }
+
+    return (
+        <div className="row">
+            <section className="side">
+                <img src={AnimasiLogin} alt="" width="50%" />
+            </section>
+            <div className="login-page">
+                <header>
+                    <img className="logo" src={Logo} alt="" width="10%" />
+                </header>
+                <h2>SELAMAT DATANG KEMBALI !</h2>
+                <InputLogin login={onLogin} />
+                <hr></hr>
+                <p className='footer-form'>Belum punya akun ?
+                    <span>{' '}<Link to='/register'>Daftar</Link></span>
+                </p>
+            </div>
+        </div>
+
+    )
 }
 
 export default LoginPage
