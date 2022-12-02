fe20b2a3cd1fae1db07afdd3f780f1962ead0db0
Gusti Ayu Istiara Bukian <istiaraayu8@gmail.com>
2022-12-02 07:17:56 +0700

    Update AddReportPage.js

--- src/script/page/AddReportPage.js
+++ src/script/page/AddReportPage.js
@@ -2,24 +2,16 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import InputAddReport from '../component/InputAddReport'
-import { login } from '../utils/network-data'
 
 function AddReportPage () {
     const navigate = useNavigate()
-    const onLogin = async ({ email, password }) => {
-        const { error } = await login({ email, password })
-
-        if (!error) {
-            navigate('/')
-        }
-    }
 
     return (
         <section className="add-report-page">
             <header>
                 <h2>LAPORKAN</h2>
             </header>
-            <InputAddReport login={onLogin} />
+            <InputAddReport />
         </section>
 
     )
