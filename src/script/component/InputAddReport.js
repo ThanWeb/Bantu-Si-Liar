430ea1cfad696c7b6dcc0e574b3d4f383c118c8b
Gusti Ayu Istiara Bukian <istiaraayu8@gmail.com>
2022-12-02 18:32:57 +0700

    Update InputAddReport.js
    update code form input halman add report

--- src/script/component/InputAddReport.js
+++ src/script/component/InputAddReport.js
@@ -1,151 +1,147 @@
-import React from 'react'
+import React, { useState } from 'react'
 import useInput from '../hooks/useInput'
 import { useNavigate } from 'react-router-dom'
 import http from '../utils/http'
 // import PropTypes from 'prop-types'
 
-function InputAddReport() {
+function InputAddReport () {
     const navigate = useNavigate()
 
-    const [data, setData] = useInput({
-        species: '',
-        color: '',
-        special_features: '',
-        location: '',
-        animal_description: '',
-        date: '',
-        animal_img: ''
-    })
-
-    // handle input
-    const handleInput = (e) => {
-        e.preventDefault()
-        setData({
-            ...data,
-            [e.target.name]: e.target.value
-        })
-    }
+    const [species, onSpeciesChange] = useInput()
+    const [color, onColorChange] = useInput()
+    const [specialFeatures, onFeaturesChange] = useInput()
+    const [location, onLocationChange] = useInput()
+    const [animalDescription, onDescriptionChange] = useInput()
+    const [date, onDateChange] = useInput()
     // handle gambar
     const [file, setFile] = useInput([])
     const handleFile = (e) => {
-        setFile({ apk_img: e.target.files[0] })
+        setFile({ animal_img: e.target.files[0] })
     }
+    const [error, setError] = useState([])
+    const [loading, setLoading] = useState(false)
+
     const onSubmitForm = async (e) => {
         e.preventDefault()
         const formData = new FormData()
         formData.append('animal_img', file.animal_img)
-        formData.append('species', data.species)
-        formData.append('color', data.color)
-        formData.append('special_features', data.special_features)
-        formData.append('location', data.location)
-        formData.append('animal_description', data.animal_description)
-        formData.append('date', data.date)
-        console.log(formData)
+        formData.append('species', onSpeciesChange.species)
+        formData.append('color', onColorChange.color)
+        formData.append('special_features', onFeaturesChange.special_features)
+        formData.append('location', onLocationChange.location)
+        formData.append('animal_description', onDescriptionChange.animal_description)
+        formData.append('date', onDateChange.date)
 
-        const req = await http.post('/user', formData)
-        if (req.data.status) {
-            alert('Data berhasil ditambahkan')
-            navigate('/homepage')
-            setData({
-                ...data,
-                species: '',
-                color: '',
-                special_features: '',
-                location: '',
-                animal_description: '',
-                date: '',
-                animal_img: ''
-            })
+        setLoading(true)
+        setError([])
+
+        try {
+            const req = await http.post('/user', formData)
+            if (req.data.status) {
+                alert('Data berhasil ditambahkan')
+                navigate('/homepage')
+            } else {
+                alert('Opss data gagal ditambahkan')
+            }
+        } catch (error) {
+            setError(error.response.data.errors)
         }
+        setLoading(false)
     }
 
     return (
-        <form onSubmit={onSubmitForm} className='input-add-report'>
-            <table >
-                <thead>
-                    <tr>
-                        <td><label>Species</label></td>
-                        <td><input
-                            type="text"
-                            name="species"
-                            value={data.species}
-                            onChange={handleInput}
-                            placeholder="species"
-                            required
-                        /></td>
-                    </tr>
-                    <tr>
-                        <td><label>Color</label></td>
-                        <input
-                            type="text"
-                            name="color"
-                            value={data.color}
-                            onChange={handleInput}
-                            placeholder="color"
-                            required
-                        />
-                    </tr>
-                    <tr>
-                        <td><label>Special Features</label></td>
-                        <textarea
-                            type="text"
-                            name="special_features"
-                            value={data.special_features}
-                            onChange={handleInput}
-                            placeholder="special features"
-                            required
-                        />
-                    </tr>
-                    <tr>
-                        <td><label>Location</label></td>
-                        <textarea
-                            type="text"
-                            name="location"
-                            value={data.location}
-                            onChange={handleInput}
-                            placeholder="location"
-                            required
-                        />
-                    </tr>
-                    <tr>
-                        <td><label>Date</label></td>
-                        <input
-                            type="text"
-                            name="date"
-                            value={data.date}
-                            onChange={handleInput}
-                            placeholder="date"
-                            required
-                        />
-                    </tr>
-                    <tr>
-                        <td><label>Animal Image</label></td>
-                        <input
-                            type="file"
-                            name="animal_img"
-                            onChange={handleFile}
-                            placeholder="File"
-                            required
-                        />
-                    </tr>
-                    <tr>
-                        <td><label>Description</label></td>
-                        <textarea
-                            type="text"
-                            name="animal_description"
-                            value={data.animal_description}
-                            onChange={handleInput}
-                            placeholder="animal description"
-                            required
-                        />
-                    </tr>
-                    <div className='button-container'>
-                        <button type="submit" className="btn-add">Add Report</button>
-                    </div>
+        <>
+            <form onSubmit={onSubmitForm} className='input-add-report'>
+                <table className='data'>
+                    <thead>
+                        {loading && <tr><td>loading...</td></tr>}
+                        <tr>
+                            <td><label>Species</label></td>
+                            <input
+                                type="text"
+                                name="species"
+                                className="wrap-input"
+                                value={species}
+                                onChange={onSpeciesChange}
+                                placeholder="species"
+                                required />
+                        </tr>
+                        <tr>
+                            <td><label>Color</label></td>
+                            <input
+                                type="text"
+                                name="color"
+                                className="wrap-input"
+                                value={color}
+                                onChange={onColorChange}
+                                placeholder="color"
+                                required />
+                        </tr>
+                        <tr>
+                            <td><label>Special Features</label></td>
+                            <textarea
+                                type="text"
+                                name="special_features"
+                                className="wrap-input"
+                                value={specialFeatures}
+                                onChange={onFeaturesChange}
+                                placeholder="special features"
+                                required />
+                        </tr>
+                        <tr>
+                            <td><label>Location</label></td>
+                            <textarea
+                                type="text"
+                                name="location"
+                                className="wrap-input"
+                                value={location}
+                                onChange={onLocationChange}
+                                placeholder="location"
+                                required />
+                        </tr>
+                        <tr>
+                            <td><label>Description</label></td>
+                            <textarea
+                                type="text"
+                                name="animal_description"
+                                className="wrap-input-desc"
+                                value={animalDescription}
+                                onChange={onDescriptionChange}
+                                placeholder="animal description"
+                                required />
+                        </tr>
+                    </thead>
+                </table>
+                <table className='data'>
+                    <thead>
+                        <tr>
+                            <td><label>Date</label></td>
+                            <input
+                                type="text"
+                                name="date"
+                                className="wrap-input"
+                                value={date}
+                                onChange={onDateChange}
+                                placeholder="date"
+                                required />
+                        </tr>
+                        <tr>
+                            <td><label>Animal Image</label></td>
+                            <input
+                                type="file"
+                                name="animal_img"
+                                className="wrap-input"
+                                onChange={handleFile}
+                                placeholder="File"
+                                required />
+                        </tr>
+                    </thead>
+                </table>
 
-                </thead>
-            </table >
-        </form >
+            </form><div className='button-container'>
+                <button type="submit" className="btn-add">Add Report</button>
+            </div>
+        </>
     )
 }
 
