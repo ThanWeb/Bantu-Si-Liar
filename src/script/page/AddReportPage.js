e8bb492f5ea8585af51d396459bae6eef907129b
Gusti Ayu Istiara Bukian <istiaraayu8@gmail.com>
2022-12-02 08:11:53 +0700

    Update AddReportPage.js

--- src/script/page/AddReportPage.js
+++ src/script/page/AddReportPage.js
@@ -1,11 +1,8 @@
 /* eslint-disable react/react-in-jsx-scope */
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
 import InputAddReport from '../component/InputAddReport'
 
 function AddReportPage () {
-    const navigate = useNavigate()
-
     return (
         <section className="add-report-page">
             <header>
