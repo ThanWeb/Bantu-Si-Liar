fe437277b0fd914832235b9e85436c2948cde35d
putriroslin <putriroslin1@gmail.com>
2022-12-03 20:54:58 +0700

    menampilkan data dari API

--- src/script/page/ArticlePage.js
+++ src/script/page/ArticlePage.js
@@ -1,18 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import getListArticle from '../utils/api-article'
-import ArticleList from '../component/ArticleList'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function ArticlePage () {
-    const [articles, setArticles] = useState([])
+    const [articles, setArticles] = useState([10])
     useEffect(() => {
-        getListArticle().then(({ data }) => {
-            setArticles(data)
-        })
+        retrieveArticle()
     }, [])
+
+    const retrieveArticle = async () => {
+        try {
+            const { data } = await axios.get('https://antekteknologi.my.id/api/rest-api-bantu-si-liar/api/list-article.php')
+            // console.log(data.data, '<== response article')
+            setArticles(data.data)
+        } catch (error) {
+            console.log(error, '<== error retrieve article')
+        }
+    }
+
     return (
         <section>
             <h1>Article Recommendations</h1>
-            <ArticleList articles={articles} />
+            {articles && articles.map((artikel) => {
+                return (
+                    <div className="article-item" key={artikel.id}>
+                        <img className="picture" src={artikel.picture} />
+                        <h2 className="title">{artikel.title}</h2>
+                        <p className="writer">{artikel.writer}</p>
+                        <p className="date">{artikel.created}</p>
+                        <p className="date">{artikel.updated}</p>
+                        <p className="thumbnail">{artikel.thumbnail}</p>
+                        <p className="body">{artikel.body}</p>
+                    </div>
+                )
+            })}
+            {/* {JSON.stringify(articles)} */}
+            {/* <p>{articles}</p> */}
+            {/* <ArticleList articles={articles} /> */}
         </section>
     )
 }
