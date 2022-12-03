a44aeb565bc16d831f166bf68fd4bfabb96f7be4
Hans <classsickpeople@gmail.com>
2022-12-04 02:23:34 +0800

    Update komponen RegisterForm

--- src/script/component/RegisterForm.js
+++ src/script/component/RegisterForm.js
@@ -2,100 +2,120 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import useInput from '../hooks/useInput'
 import { useNavigate } from 'react-router-dom'
-import paginationSlider from '../utils/pagination-slider'
 import STATIC from '../globals/static-data'
+import showPictureOption from '../utils/show-picture-option'
 
 const RegisterForm = ({ register }) => {
     const navigate = useNavigate()
-    const [pagination, togglePagination] = useState('')
+    const [cityOption, setCityOption] = useState([])
 
-    const [username, onUsernameChange] = useInput()
-    const [email, onEmailChange] = useInput()
-    const [password, onPasswordChange] = useInput()
-    const [profile, onProfileChange] = useInput()
-    const [name, onNameChange] = useInput()
-    const [phone, onPhoneChange] = useInput()
-    const [province, onProvinceChange] = useInput()
-    const [city, onCityChange] = useInput()
-    const [address, onAddressChange] = useInput()
+    const [username, setUsername] = useInput()
+    const [email, setEmail] = useInput()
+    const [password, setPassword] = useInput()
+    const [profile, setProfile] = useState('')
+    const [name, setName] = useInput()
+    const [phone, setPhone] = useInput()
+    const [province, setProvince] = useInput('- Please Select -')
+    const [city, setCity] = useState('- Please Select -')
+    const [address, setAddress] = useInput()
 
     useEffect(() => {
-        getPagination()
-    }, [])
+        changeCityOption()
+    }, [province])
 
     const onSubmitHandler = () => {
         register({ username, email, password, profile, name, phone, province, city, address })
         navigate('/')
     }
 
-    const getPagination = () => {
-        const currentPagination = sessionStorage.getItem('pagination') || 'prev'
-        togglePagination(currentPagination)
-        sessionStorage.setItem('pagination', currentPagination)
+    const changeCityOption = () => {
+        setCity('')
+        if (province === '- Please Select -') {
+            setCityOption([])
+        } else {
+            STATIC.places.forEach(place => {
+                if (place.province === province) {
+                    setCityOption(place.cities)
+                }
+            })
+        }
     }
 
-    const setPagination = () => {
-        const prevPagination = sessionStorage.getItem('pagination')
-        const newPagination = prevPagination === 'next' ? 'prev' : 'next'
-        togglePagination(newPagination)
-        sessionStorage.setItem('pagination', newPagination)
-        paginationSlider()
+    const changeSelectedCity = () => {
+        const city = document.querySelector('#city').value
+        setCity(city)
+    }
+
+    const setPicture = (picture) => {
+        setProfile(picture)
     }
 
     return (
         <form onSubmit={onSubmitHandler} className='register'>
-            <div className='pagination'>
+            <div>
                 <div>
-                    <label htmlFor="username">Username</label>
-                    <input type='text' value={username} onChange={onUsernameChange} required />
+                    <label htmlFor='username'>Username</label>
+                    <input id='username' type='text' value={username} onChange={setUsername} required />
                 </div>
                 <div>
-                    <label htmlFor="email">Email</label>
-                    <input type='email' value={email} onChange={onEmailChange} required />
+                    <label htmlFor='email'>Email</label>
+                    <input id='email' type='email' value={email} onChange={setEmail} required />
                 </div>
                 <div>
-                    <label htmlFor="password">Password</label>
-                    <input type='password' autoComplete='current-password' value={password} onChange={onPasswordChange} required />
-                </div>
-                <div>
-                    <span onClick={setPagination}>
-                        {pagination === 'next' ? 'PREV' : 'NEXT'}
-                    </span>
+                    <label htmlFor='password'>Password</label>
+                    <input id='password' type='password' minLength='8' autoComplete='current-password' value={password} onChange={setPassword} required />
                 </div>
             </div>
-            <div className='pagination hidden'>
+            <div>
                 <div>
-                    <label htmlFor="name">Nama</label>
-                    <input type='text' value={name} onChange={onNameChange} required />
+                    <label htmlFor='name'>Nama</label>
+                    <input id='name' type='text' value={name} onChange={setName} required />
                 </div>
                 <div>
-                    <label htmlFor="phone">No Handphone</label>
-                    <input type='tel' value={phone} onChange={onPhoneChange} required />
+                    <label htmlFor='phone'>No Handphone</label>
+                    <input id='phone' type='tel' value={phone} onChange={setPhone} required />
                 </div>
                 <div>
-                    <label htmlFor="province">Provinsi</label>
-                    <select type='select' value={province} onChange={onProvinceChange} required>
+                    <label>Provinsi</label>
+                    <select type='select' value={province} onChange={setProvince} required>
+                        <option>- Please Select -</option>
                         {
-                            STATIC.PROVINCE.map((province, index) =>
-                                <option key={index}>{ province }</option>
+                            STATIC.places.map((place, index) =>
+                                <option key={index}>{ place.province }</option>
                             )
                         }
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="city">Kabupaten/Kota</label>
-                    <select type='select' value={city} onChange={onCityChange} required />
+                    <label>Kabupaten/ Kota</label>
+                    <select id='city' type='select' value={city} onChange={changeSelectedCity} required>
+                        <option>- Please Select -</option>
+                        {
+                            cityOption.map((city, index) =>
+                                <option key={index}>{ city }</option>
+                            )
+                        }
+                    </select>
                 </div>
                 <div>
-                    <label htmlFor="address">Alamat Lengkap</label>
-                    <input type='text' className='address' value={address} onChange={onAddressChange} required />
+                    <label htmlFor='address'>Alamat Lengkap</label>
+                    <input id='address' type='text' className='address' value={address} onChange={setAddress} required />
                 </div>
                 <div>
-                    <label htmlFor="profile">Profile Picture</label>
-                    <select type='url' value={profile} onChange={onProfileChange} required />
+                    {
+                        profile !== '' &&
+                            <img src={profile} alt={'Selected Picture'} />
+                    }
+                    <span onClick={showPictureOption}>Choose Picture</span>
+                    <div className='hidden picture-list' id='picture-list'>
+                        {
+                            STATIC.profilePicture.map((picture, index) =>
+                                <img key={index} className='picture-option' src={picture} alt={`Picture ${index + 1}`} onClick={() => setPicture(picture)} />
+                            )
+                        }
+                    </div>
                 </div>
                 <div>
-                    <span onClick={setPagination}>{pagination === 'next' ? 'PREV' : 'NEXT'}</span>
                     <button type='submit' className='btn-daftar'>SIGN UP</button>
                 </div>
             </div>
