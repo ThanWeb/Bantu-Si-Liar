3f81b46ae68e7e1a12d3b0a5072d8255615298ef
Hans <classsickpeople@gmail.com>
2022-12-03 18:25:56 +0800

    Menghapus fitur pagination slider

--- src/script/utils/pagination-slider.js
+++
@@ -1,8 +0,0 @@
-const paginationSlider = () => {
-    const paginations = document.querySelectorAll('.pagination')
-    paginations.forEach(pagination => {
-        pagination.classList.toggle('hidden')
-    })
-}
-
-export default paginationSlider
