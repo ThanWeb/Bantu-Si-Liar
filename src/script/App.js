4064cfebdbfb044f392f79fbbb837463b73085f4
Hans <classsickpeople@gmail.com>
2022-12-05 23:49:22 +0800

    Menambahkan beberapa fungsi untuk fitur login dan logout

--- src/script/App.js
+++ src/script/App.js
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navigation from './component/Navigation'
 import HomePage from './page/HomePage'
 import ProfilePage from './page/ProfilePage'
 import LoginPage from './page/LoginPage'
 import RegisterPage from './page/RegisterPage'
+import ReportPage from './page/ReportPage'
+import { getLoggedId, getProfile, removeLoggedId } from './utils/network-data'
 
 function App () {
+    const [authedUser, setAuthedUser] = useState('')
+    const [profile, setProfile] = useState({})
+
+    useEffect(() => {
+        intiateProfileData()
+    }, [])
+
+    const intiateProfileData = async () => {
+        const id = getLoggedId()
+        if (id) {
+            setAuthedUser(id)
+            const { data } = await getProfile(id)
+            setProfile(data)
+        }
+    }
+
+    const onLoginHandler = (id) => {
+        setAuthedUser(id)
+    }
+
+    const onLogoutHandler = () => {
+        removeLoggedId()
+        setAuthedUser('')
+    }
+
     return (
         <div>
-            <header className="nav-container">
-                <Navigation />
+            <header className='nav-container'>
+                <Navigation loginStatus={authedUser} profileData={profile} logout={onLogoutHandler}/>
             </header>
             <main>
                 <Routes>
                     <Route path='/' element={<HomePage />} />
-                    <Route path='login' element={<LoginPage />} />
+                    <Route path='login' element={<LoginPage loginSuccess={onLoginHandler}/>} />
                     <Route path='register' element={<RegisterPage />} />
-                    <Route path='profile' element={<ProfilePage />} />
+                    <Route path='profile' element={<ProfilePage data={profile} loginStatus={authedUser}/>} />
+                    <Route path='report' element={<ReportPage loginStatus={authedUser}/>} />
                 </Routes>
             </main>
             <footer>
