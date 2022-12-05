2d1267ce5930b555c861067b5c87dfcff696e896
Hans <classsickpeople@gmail.com>
2022-12-05 23:48:19 +0800

    Menambakan endpoint untuk API profile

--- src/script/globals/api-endpoint.js
+++ src/script/globals/api-endpoint.js
@@ -2,7 +2,8 @@ import CONFIG from './config'
 
 const API_ENDPOINT = {
     REGISTER: `${CONFIG.BASE_URL}register.php`,
-    LOGIN: `${CONFIG.BASE_URL}login.php`
+    LOGIN: `${CONFIG.BASE_URL}login.php`,
+    PROFILE: `${CONFIG.BASE_URL}profile.php?id=`
 }
 
 export default API_ENDPOINT
