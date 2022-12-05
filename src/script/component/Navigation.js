4030755215c66a86b6d537300b82a0e0c459f80a
Hans <classsickpeople@gmail.com>
2022-12-05 23:48:56 +0800

    Menambahkan logika kondisional pada navigation

--- src/script/component/Navigation.js
+++ src/script/component/Navigation.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
-const Navigation = () => {
+const Navigation = ({ loginStatus, profileData, logout }) => {
+    if (loginStatus === '') {
+        return (
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='login'>Login</Link>
+                <Link to='report'>Report</Link>
+            </nav>
+        )
+    }
+
     return (
         <nav>
             <Link to='/'>Home</Link>
-            <Link to='register'>Register</Link>
-            <Link to='login'>Login</Link>
-            <Link to='profile'>Profile</Link>
+            <Link to='profile'>
+                <img src={profileData.picture} alt={profileData.name}/>
+                <span>{profileData.name}</span>
+            </Link>
+            <Link to='report'>Report</Link>
+            <Link to='/' onClick={logout}>Log Out</Link>
         </nav>
     )
+}
+
+Navigation.propTypes = {
+    loginStatus: PropTypes.string,
+    profileData: PropTypes.object,
+    logout: PropTypes.func
 }
 
 export default Navigation
