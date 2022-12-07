32c2e690d25d5fc53928854bf1cf593689b2ebb2
Hans <classsickpeople@gmail.com>
2022-12-07 12:04:24 +0800

    Update homepage

--- src/script/page/Homepage.js
+++ src/script/page/Homepage.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Jumbotron from '../component/Jumbotron'
+import AboutSection from '../component/AboutSection'
+import ReportsSection from '../component/ReportsSection'
+import ArticlesSection from '../component/ArticlesSection'
+import { getArticles } from '../utils/network-data'
 
 const HomePage = () => {
-    return <h1>HomePage</h1>
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        loadArticles()
+    }, [])
+
+    const loadArticles = async () => {
+        const { data } = await getArticles()
+        setArticles(data)
+    }
+
+    return (
+        <div className='home-page'>
+            <Jumbotron/>
+            <AboutSection/>
+            <ReportsSection/>
+            <ArticlesSection articleList={articles}/>
+        </div>
+    )
 }
 
 export default HomePage
