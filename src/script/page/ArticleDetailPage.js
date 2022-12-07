74bc71a6ef76b7268d918dac54c1d3fd32344f1a
Hans <classsickpeople@gmail.com>
2022-12-07 23:26:38 +0800

    Menambahkan article detail page

--- src/script/page/ArticleDetailPage.js
+++ src/script/page/ArticleDetailPage.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import getArticleBody from '../utils/getArticleBody'
+import { useParams } from 'react-router-dom'
+import { getSingleArticle } from '../utils/network-data'
 
 const ArticleDetailPage = () => {
-    const arrayOfText = getArticleBody(body)
+    const { id } = useParams()
+    const [article, setArticle] = useState({})
+
+    useEffect(() => {
+        getArticleData()
+    }, [])
+
+    const getArticleData = async () => {
+        const { data } = await getSingleArticle(id)
+        setArticle(data)
+    }
+
     return (
         <div>
+            <h2>{article.title}</h2>
+            <img src={article.picture} alt='Article Image'/>
+            <p className='italic-text'>Ditulis oleh ({article.writer}) pada {article.created}</p>
+            <p className='italic-text'>Artikel terakhir kali diubah pada {article.updated}</p>
             {
-                arrayOfText.contents.map((text, index) => {
-                    return arrayOfText.tags[index - 1] === 'h' ? <h3>{text}</h3> : <p>{text}</p>
-                })
+                article.body
+                    ? getArticleBody(article.body).contents.map((text, index) => {
+                        return getArticleBody(article.body).tags[index - 1] === 'h' ? <h3 key={index}>{text}</h3> : <p key={index}>{text}</p>
+                    })
+                    : <p>None</p>
             }
+            <p>Sumber Artikel: <a href={article.reference} target='_blank' rel="noreferrer">{article.title}</a></p>
         </div>
     )
 }
