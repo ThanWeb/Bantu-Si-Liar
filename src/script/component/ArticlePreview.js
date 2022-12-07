9abdcad0ae46fe137b21183342c292c9f8b0aa9d
Hans <classsickpeople@gmail.com>
2022-12-07 23:26:06 +0800

    Menambahkan komponen article preview

--- src/script/component/ArticlePreview.js
+++ src/script/component/ArticlePreview.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const ArticlesSection = ({ articleData }) => {
     return (
         <div className='article-preview'>
             <img src={articleData.picture}/>
-            <h3>{articleData.title}</h3>
+            <Link to={`/article-detail/${articleData.id}`}>
+                <h3>{articleData.title}</h3>
+            </Link>
             <p>{articleData.writer}, {articleData.created}</p>
             <p>{articleData.thumbnail}</p>
         </div>
