1d5df66b50d99591b4711ce8053a1bbcbc79a1ba
Hans <classsickpeople@gmail.com>
2022-12-07 23:24:13 +0800

    Mengubah page list artikel

---
+++ src/script/page/ArticleListPage.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react'
+import { getArticles } from '../utils/network-data'
+import ArticlePreview from '../component/ArticlePreview'
+
+function ArticleListPage () {
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        retrieveArticle()
+    }, [])
+
+    const retrieveArticle = async () => {
+        const { data } = await getArticles()
+        setArticles(data)
+    }
+
+    return (
+        <div className="article-list-page">
+            {
+                articles.map((article, index) => <ArticlePreview key={index} articleData={article}/>)
+            }
+        </div>
+    )
+}
+
+export default ArticleListPage
