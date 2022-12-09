54ef97b4eccda7da976920d22ecc8b4b0daf7021
Hans <classsickpeople@gmail.com>
2022-12-09 23:10:13 +0800

    Styling komponen footer

--- src/script/component/Footer.js
+++ src/script/component/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { BsWhatsapp, BsInstagram, BsFacebook } from 'react-icons/bs'
 
 const Footer = () => {
     const findYear = () => {
@@ -10,9 +10,23 @@ const Footer = () => {
 
     return (
         <footer className='footer'>
-            <div>
-                <img src='public/jumbotron-image.png' alt='Jumbotron Image'/>
-                <Link to='create-report'>Laporkan</Link>
+            <h3>Media Sosial</h3>
+            <div className='social-media'>
+                <div>
+                    <a href='https://whatsapp.com' target='_blank' rel='noreferrer'>
+                        <BsWhatsapp/>
+                    </a>
+                </div>
+                <div>
+                    <a href='https://facebook.com' target='_blank' rel='noreferrer'>
+                        <BsFacebook/>
+                    </a>
+                </div>
+                <div>
+                    <a href='https://instagram.com' target='_blank' rel='noreferrer'>
+                        <BsInstagram/>
+                    </a>
+                </div>
             </div>
             <div>
                 <p>&#169; {findYear()} Bantu Si Liar Organization. All Right Reserved</p>
