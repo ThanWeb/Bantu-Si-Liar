5f9b7004f36adca13b27e226cdb7358eda3f2805
Hans <classsickpeople@gmail.com>
2022-12-10 02:19:57 +0800

    Styling report detail page

--- src/script/page/ReportDetailPage.js
+++ src/script/page/ReportDetailPage.js
@@ -19,16 +19,16 @@ const ReportDetailPage = () => {
     return (
         <div className='report-detail-page'>
             <img src={`${CONFIG.BASE_IMAGE_URL}${report.picture}`} alt='Report Image'/>
-            <p>Laporan dibuat pada {report.date}</p>
-            <h3>Jenis Hewan {report.animal}</h3>
-            <p>Warna Bulu {report.color}</p>
+            <p>Laporan dibuat pada <span className='highlight'>{report.date}</span></p>
+            <h3>Jenis Hewan <span className='highlight'>{report.animal}</span></h3>
+            <p>Warna Bulu <span className='highlight'>{report.color}</span></p>
             <div>
                 <p>Deskripsi Lengkap</p>
-                <p>{report.characteristic}</p>
+                <p><span className='highlight'>{report.characteristic}</span></p>
             </div>
-            <p>Lokasi terakhir terlihat di {report.location}, {report.city}, {report.province}</p>
-            <p>Laporan dibuat oleh {report.reporter}</p>
-            <p>Nomor telepon pelapor {report.phone}</p>
+            <p>Lokasi terakhir terlihat di <span className='highlight'>{report.location}, {report.city}, {report.province}</span></p>
+            <p>Laporan dibuat oleh <span className='highlight'>{report.reporter}</span></p>
+            <p>Nomor telepon pelapor <span className='highlight'>{report.phone}</span></p>
         </div>
     )
 }
