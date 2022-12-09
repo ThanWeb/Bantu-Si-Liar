148cdd80a9603c3182d747707c3272382954c3c3
Hans <classsickpeople@gmail.com>
2022-12-09 10:23:57 +0800

    Mengubah tampilan report detail page

--- src/script/page/ReportDetailPage.js
+++ src/script/page/ReportDetailPage.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { getSingleReport } from '../utils/network-data'
 
 const ReportDetailPage = () => {
+    const { id } = useParams()
+    const [report, setReport] = useState({})
+
+    useEffect(() => {
+        getReportData()
+    }, [])
+
+    const getReportData = async () => {
+        const { data } = await getSingleReport(id)
+        setReport(data)
+    }
+
     return (
         <div className='report-detail-page'>
-            <h1>ReportDetailPage</h1>
+            <img src={report.picture} alt='Report Image'/>
+            <p>Laporan dibuat pada {report.date}</p>
+            <h3>Jenis Hewan {report.animal}</h3>
+            <p>Warna Bulu {report.color}</p>
+            <div>
+                <p>Deskripsi Lengkap</p>
+                <p>{report.characteristic}</p>
+            </div>
+            <p>Lokasi terakhir terlihat di {report.location}, {report.city}, {report.province}</p>
+            <p>Laporan dibuat oleh {report.reporter}</p>
+            <p>Nomor telepon pelapor {report.phone}</p>
         </div>
     )
 }
