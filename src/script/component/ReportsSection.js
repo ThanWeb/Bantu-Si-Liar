d7b9a085ee2310517d200e2d8d7971987f80e861
Hans <classsickpeople@gmail.com>
2022-12-09 14:14:03 +0800

    Mengubah tampilan artikel dan report section

--- src/script/component/ReportsSection.js
+++ src/script/component/ReportsSection.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import ReportPreview from './ReportPreview'
+import { Link } from 'react-router-dom'
 
-const ReportsSection = () => {
+const ReportsSection = ({ reportList }) => {
+    const getLatestReports = reports => {
+        const length = reportList.length < 3 ? reportList.length : 3
+        const selectedReports = []
+        for (let i = 0; i < length; i++) {
+            const report = reports[i]
+            selectedReports.push(<ReportPreview key={i} reportData={report}/>)
+        }
+        return selectedReports
+    }
+
     return (
         <div className='reports-section'>
-            <p>ReportsSection</p>
+            <h2>Laporan Terbaru</h2>
+            {
+                getLatestReports(reportList)
+            }
+            <div>
+                <Link to='/report-list'>Lihat Semua</Link>
+            </div>
         </div>
     )
+}
+
+ReportsSection.propTypes = {
+    reportList: PropTypes.arrayOf(PropTypes.object)
 }
 
 export default ReportsSection
