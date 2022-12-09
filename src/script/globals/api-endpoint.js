622be21e7fa4741eda3e32801f85d072fece7d81
Hans <classsickpeople@gmail.com>
2022-12-09 10:23:12 +0800

    Menambahkan endpoint untuk API single report

--- src/script/globals/api-endpoint.js
+++ src/script/globals/api-endpoint.js
@@ -6,7 +6,8 @@ const API_ENDPOINT = {
     PROFILE: `${CONFIG.BASE_URL}profile.php?id=`,
     ARTICLE_LIST: `${CONFIG.BASE_URL}list-article.php`,
     SINGLE_ARTICLE: `${CONFIG.BASE_URL}single-article.php?id=`,
-    REPORT_LIST: `${CONFIG.BASE_URL}list-report.php`
+    REPORT_LIST: `${CONFIG.BASE_URL}list-report.php`,
+    SINGLE_REPORT: `${CONFIG.BASE_URL}single-report.php?id=`
 }
 
 export default API_ENDPOINT
