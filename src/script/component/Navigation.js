0e2139637958c2495341fd7aeee2166e5bfe0b4b
Hans <classsickpeople@gmail.com>
2022-12-09 21:29:36 +0800

    Mengubah komponen navigation

--- src/script/component/Navigation.js
+++ src/script/component/Navigation.js
@@ -1,31 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { FiAlignJustify } from 'react-icons/fi'
 
 const Navigation = ({ loginStatus, profileData, logout }) => {
+    const toggleDisplayDrawer = () => {
+        if (document.querySelector('.drawer')) {
+            document.querySelector('.drawer').classList.toggle('show')
+        }
+    }
+
     if (loginStatus === '') {
         return (
             <nav className='navigation'>
-                <Link to='/'>Beranda</Link>
-                <Link to='login'>Masuk</Link>
-                <Link to='create-report'>Buat Laporan</Link>
-                <Link to='report-list'>Daftar Laporan</Link>
-                <Link to='article-list'>Daftar Artikel</Link>
+                <div>
+                    <Link to='/' className='container-icon'><h1 className='app-title'>Bantu Si Liar</h1></Link>
+                </div>
+                <div className='container-icon' onClick={toggleDisplayDrawer}>
+                    <FiAlignJustify className='icon'/>
+                </div>
+                <div className='drawer' id='drawer' onClick={toggleDisplayDrawer}>
+                    <Link to='login'>Masuk</Link>
+                    <Link to='create-report'>Buat Laporan</Link>
+                    <Link to='report-list'>Daftar Laporan</Link>
+                    <Link to='article-list'>Daftar Artikel</Link>
+                    <div className='profile-icon'>
+                        <img className='logo' src='public/logo-transparent.png' alt='Logo Bantu Si Liar'/>
+                    </div>
+                </div>
             </nav>
         )
     }
 
     return (
         <nav className='navigation'>
-            <Link to='/'>Home</Link>
-            <Link to='profile'>
-                <img src={profileData.picture} alt={profileData.name}/>
-                <span>{profileData.name}</span>
-            </Link>
-            <Link to='create-report'>Buat Laporan</Link>
-            <Link to='report-list'>Daftar Laporan</Link>
-            <Link to='article-list'>Daftar Artikel</Link>
-            <Link to='/' onClick={logout}>Keluar</Link>
+            <div>
+                <Link to='/' className='container-icon'><h1 className='app-title'>Bantu Si Liar</h1></Link>
+            </div>
+            <div className='container-icon' onClick={toggleDisplayDrawer}>
+                <FiAlignJustify className='icon'/>
+            </div>
+            <div className='drawer' id='drawer' onClick={toggleDisplayDrawer}>
+                <Link to='profile'>
+                    <div className='profile-icon'>
+                        <img src={profileData.picture} alt={profileData.name} className='profile-image'/>
+                        <span>{profileData.name}</span>
+                    </div>
+                </Link>
+                <Link to='create-report'>Buat Laporan</Link>
+                <Link to='report-list'>Daftar Laporan</Link>
+                <Link to='article-list'>Daftar Artikel</Link>
+                <Link to='/' onClick={(logout)}>Keluar</Link>
+            </div>
         </nav>
     )
 }
